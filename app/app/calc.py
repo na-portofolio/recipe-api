f6d69eed48db90230e79cb5b0cb31703d391ea31
na-portofolio <dsc.uho@gmail.com>
2022-11-13 20:08:34 +0700

    add another unit test

--- app/app/calc.py
+++ app/app/calc.py
@@ -6,3 +6,8 @@ Calculator function
 def add(x, y):
     """Add x and y and return result."""
     return x + y
+
+
+def substract(x, y):
+    """substract x and y and return result."""
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -14,3 +14,9 @@ class CalcTests(SimpleTestCase):
         res = calc.add(2, 3)
 
         self.assertEqual(res, 5)
+
+    def test_substract_number(self):
+        """Test substract numbers together."""
+        res = calc.substract(2, 4)
+
+        self.assertEqual(res, 2)
